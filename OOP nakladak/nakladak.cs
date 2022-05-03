992623cd5f0698392a330e85d142cf34f2529d8b
Romana Černá <Cerna.Romana@stud.vassboskovice.cz>
2022-05-03 09:00:26 +0200

    Přidejte soubory projektu.

---
+++ OOP nakladak/nakladak.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_nakladak
+{
+    internal class nakladak
+    {
+        public string Name { get; set; }
+        public int Nosnost { get; set; }
+        public int Naklad { get; set; }
+        public int Objemnadrze { get; set; }
+        public int Sotrebapaliva { get; set; }
+        public int Celkovavzdalenost { get; set; }
+
+        public nakladak()
+        {
+            Nosnost = 66;
+            Naklad = 100;
+            Objemnadrze = 70;
+            Sotrebapaliva = 150;
+            Celkovavzdalenost = 156;
+        }
+    }
+}
