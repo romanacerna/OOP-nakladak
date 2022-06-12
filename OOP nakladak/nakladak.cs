e99e4bef35c5195c68d86252ba1a299631c960e9
romanacerna <90243118+romanacerna@users.noreply.github.com>
2022-06-12 22:12:20 +0200

    Update nakladak.cs

--- OOP nakladak/nakladak.cs
+++ OOP nakladak/nakladak.cs
@@ -1,27 +1,95 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace OOP_nakladak
+namespace Romana
 {
-    internal class nakladak
+    public class Nakladak
     {
-        public string Name { get; set; }
-        public int Nosnost { get; set; }
-        public int Naklad { get; set; }
-        public int Objemnadrze { get; set; }
-        public int Spotrebapaliva { get; set; }
-        public int Celkovavzdalenost { get; set; }
 
-        public nakladak()
+
+        public int MaxNosnost { get; set; }
+        public int MaxPalivo { get; set; }
+        public int Spotreba { get; set; }
+        public int Vzdalenost { get; set; }
+        public int Palivo { get; set; }
+        public int Naklad { get; set; }
+        
+        public Nakladak()
         {
-            Nosnost = 66;
-            Naklad = 100;
-            Objemnadrze = 70;
-            Spotrebapaliva = 150;
-            Celkovavzdalenost = 156;
+            MaxNosnost = 3000;
+            MaxPalivo = 150;
+            Naklad = 0;
+            Vzdalenost = 0;
+            Palivo = 150;
+            Spotreba = 0;
+        }
+        public void Nalozit()
+        {
+            if(Naklad < 3000)
+            {
+                Naklad += 1000;
+            }
+            else if(Naklad == 3000)
+            {
+                Naklad += 0;
+            }
+        }
+        public void Vylozit()
+        {
+            if (Naklad > 0)
+            {
+                Naklad -= 1000;
+            }
+            else if (Naklad == 0)
+            {
+                Naklad -= 0;
+            }
+        }
+        public void Jizda()
+        {
+            
+            if (Palivo > 0)
+            {
+                Vzdalenost += 100;
+                Palivo -= 30;
+            }
+            
+            else if(Palivo == 0)
+            {
+                Vzdalenost += 0;
+            }
+        }
+
+        public void SpoTreba()
+        {
+            if (Palivo == 0)
+            {
+                Spotreba = 0;
+            }
+            else if (Naklad >= 2000)
+            {
+                Spotreba = 50;
+            }
+            else if (Naklad < 2000)
+            {
+                Spotreba = 40;
+            }
+            else if(Naklad <= 1000)
+            {
+                Spotreba = 30;
+            }
+        }
+        public void Natankovat()
+        {
+            if(Palivo < 150)
+            {
+                Palivo += 30;
+            }
+            if(Palivo == 150)
+            {
+                Palivo += 0;
+            }
         }
     }
 }
